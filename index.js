5baefc89a96e8b876bfbf72f2493caa639f6335a
Jeremy <markjeremyj@gmail.com>
2025-06-12 19:57:57 +0300

    places filter function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names, list) {
+    return names.filter(name => name.toLowerCase() === list.toLowerCase());
+}
+function fuzzyMatch(names, letter){
+    return names.filter(name => name[0].toLowerCase() === letter[0].toLowerCase());
+
+}
+function matchName(names , list){
+    return names.filter(name => name.name === list);
+
+}
